b3a545d1d02a717e4003b0dee7dbe44fa1dec89e
pw0908 <pjwalker@caltech.edu>
2023-10-18 15:24:33 -0700

    Add try-catch for openmm module

--- src/mdhelper/__init__.py
+++ src/mdhelper/__init__.py
@@ -11,4 +11,11 @@ ArrayLike = TypeVar("ArrayLike", list, np.ndarray, tuple)
 VERSION = "0.0.2"
 __all__ = ["algorithm", "analysis", "fit", "openmm", "plot"]
 
-from . import algorithm, analysis, fit, openmm, plot # noqa: E402
+from . import algorithm, analysis, fit, plot # noqa: E402
+
+try:
+    import openmm
+    from . import openmm
+except ImportError:
+    Warning("openmm not installed. Some features may not work.")
+    pass
